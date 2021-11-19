0e5a640c46cf178e76c42f5bbb2e078d6edd6a70
FA1312 <faldrovandi83@gmail.com>
2021-11-19 18:12:27 +0100

    lab done

--- index.js
+++ index.js
@@ -1,17 +1,56 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    this.length += 1;
+    this.items.push(item);
+    this.items.sort(function (a, b) {
+      return a - b;
+    });
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos > this.items.length) {
+      throw new Error("OutOfBounds");
+    }
+    return this.items[pos];
+  }
 
-  max() {}
+  max() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    return this.items[this.items.length - 1];
+  }
 
-  min() {}
+  min() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    return this.items[0];
+  }
 
-  sum() {}
+  sum() {
+    let suma = 0;
+    for (var i = 0; i < this.items.length; i++) {
+      suma += this.items[i];
+    }
+    return suma;
+  }
 
-  avg() {}
+  avg() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    let suma = 0;
+    for (var i = 0; i < this.items.length; i++) {
+      suma += this.items[i];
+    }
+    return suma / this.items.length;
+  }
 }
 
 module.exports = SortedList;
